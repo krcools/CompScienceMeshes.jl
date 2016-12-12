05e2925366538f6495d03c96742d69fba6cd173b
Kristof Cools <krcools@gmail.com>
2016-12-12 17:59:34 +0000

    cellpairs works on junctions - tested

--- test/test_jctweld.jl
+++ test/test_jctweld.jl
@@ -21,3 +21,18 @@ G = weld(G1, G2, G3)
 
 @test numcells(G) == numcells(G1) + numcells(G2) + numcells(G3)
 @test numvertices(G) == 5
+
+edges = skeleton(G,1)
+pairs = cellpairs(G, edges, dropjunctionpair=true)
+
+@test size(pairs) == (2,8)
+
+mesh = meshrectangle(1.0, 1.0, 0.5)
+edges = skeleton(mesh,1)
+cps = cellpairs(mesh, edges)
+@test size(cps) == (2,16)
+
+# width, height = 1.0, 0.5; h = 0.1
+# mesh = meshrectangle(width, height, h)
+# edges = skeleton(mesh,1)
+# cps = cellpairs(mesh, edges, dropjunctionpair=true)
