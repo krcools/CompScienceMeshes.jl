0a84fd6f3c15279266e29602ab72b117c2b3b576
Fredrik Ekre <fredrik.ekre@chalmers.se>
2018-04-27 12:36:26 +0200

    The `julia` keyword argument to `Documenter.deploydocs` will be required in the future instead of automatically defaulting to `julia = "nightly" (see https://github.com/JuliaDocs/Documenter.jl/pull/712, https://github.com/JuliaDocs/Documenter.jl/issues/526).
    This commit adds `julia = "nightly"` to `Documenter.deploydocs`.

--- docs/make.jl
+++ docs/make.jl
@@ -2,6 +2,7 @@ using Documenter, CompScienceMeshes
 
 makedocs(clean=false)
 deploydocs(
+    julia = "nightly",
     deps = Deps.pip("mkdocs", "python-markdown-math"),
     repo = "github.com/krcools/CompScienceMeshes.jl.git",
 )
