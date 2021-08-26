57ae8d2c787d0c7f894b46c605d2a11245677b65
Kristof Cools <krcools@gmail.com>
2021-08-26 18:04:50 +0200

    wireframe visualisation optimised

--- src/plotlyjs_glue.jl
+++ src/plotlyjs_glue.jl
@@ -1,5 +1,6 @@
 export patch
 export normalcones
+export wireframe
 
 function __init__()
     @require PlotlyJS="f0f68f2c-4968-5e81-91da-67840de0976a" begin
@@ -44,7 +45,7 @@ function __init__()
             )
         end
 
-        @eval function normalcones(mesh::AbstractMesh)
+        @eval function normalcones(mesh::AbstractMesh; sizeref=2)
             normals = [normal(chart(mesh,cell)) for cell in mesh]
             centers = [cartesian(center(chart(mesh,cell))) for cell in mesh]
             x = getindex.(centers,1)
@@ -53,7 +54,27 @@ function __init__()
             u = getindex.(normals,1)
             v = getindex.(normals,2)
             w = getindex.(normals,3)
-            PlotlyJS.cone(x=x,y=y,z=z,u=u,v=v,w=w,sizemode="absolute", sizeref=2)
+            PlotlyJS.cone(x=x,y=y,z=z,u=u,v=v,w=w,sizemode="absolute", sizeref=sizeref)
+        end
+
+        @eval function wireframe(edges; width=1, color="rgb(0,0,0)")
+            T = coordtype(edges)
+            x = T[]
+            y = T[]
+            z = T[]
+            for edge in edges
+                chrt = chart(edges, edge)
+                v1, v2 = chrt.vertices
+                append!(x, [v1[1],v2[1],NaN])
+                append!(y, [v1[2],v2[2],NaN])
+                append!(z, [v1[3],v2[3],NaN])
+            end
+            return PlotlyJS.scatter3d(x=x,y=y,z=z,mode="lines",
+                line=PlotlyJS.attr(
+                    color=color,
+                    width=width
+                )
+            )
         end
     end
 end
