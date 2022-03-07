46466ac540dd9b68c8999d137127f66048f6b842
Kristof Cools <krcools@gmail.com>
2022-03-07 14:45:42 +0100

    patch with control over color_axis

--- src/plotlyjs_glue.jl
+++ src/plotlyjs_glue.jl
@@ -4,45 +4,40 @@ export wireframe
 
 function __init__()
     @require PlotlyJS="f0f68f2c-4968-5e81-91da-67840de0976a" begin
-        @eval import DelimitedFiles
-        @eval fn = joinpath(dirname(@__FILE__),"..","examples","cm.csv")
-        @eval cm = mapslices(r->(r[1],r[2],r[3]), DelimitedFiles.readdlm(fn, ','), dims=[2])
-        @eval function patch(Γ::AbstractMesh, fcr=nothing, caxis=nothing; showscale=true)
+        # @eval import DelimitedFiles
+        # @eval fn = joinpath(dirname(@__FILE__),"..","examples","cm.csv")
+        # @eval cm = mapslices(r->(r[1],r[2],r[3]), DelimitedFiles.readdlm(fn, ','), dims=[2])
+        @eval function patch(Γ::AbstractMesh, fcr=nothing; caxis=nothing, showscale=true)
 
             v = vertexarray(Γ)
             c = cellarray(Γ)
-
+        
             x = v[:,1];    y = v[:,2];    z = v[:,3]
             i = c[:,1].-1; j = c[:,2].-1; k = c[:,3].-1
-
+        
             if fcr == nothing
                 a = [cartesian(center(chart(Γ,cell)))[3] for cell in cells(Γ)]
             else
                 a = fcr
             end
-
+        
             m, M = extrema(a)
             if caxis != nothing
                 m, M = caxis
             end
-
-            cs = map(eachindex(cm)) do i
-                r, g, b = cm[i]
-                R = floor(Int,255*r)
-                G = floor(Int,255*g)
-                B = floor(Int,255*b)
-                I = (i-1)/(length(cm)-1)
-                [I, "rgb($R,$G,$B)"]
-            end
-
+        
             s = PlotlyJS.mesh3d(;
                 x=x, y=y, z=z,
                 i=i, j=j, k=k,
+                
                 intensitymode="cell",
                 intensity=a,
-                colorscale=cs,
+                colorscale="Viridis",
                 showscale=showscale,
+                cmin=m,
+                cmax=M
             )
+
         end
 
         @eval function normalcones(mesh::AbstractMesh; sizeref=2)
