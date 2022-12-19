954369707a18db508360f6333469597473fa2a1d
lvchien <v.chienle@gmail.com>
2022-12-19 02:11:07 +0100

    Add square torus with 4 holes

--- src/primitives.jl
+++ src/primitives.jl
@@ -353,7 +353,7 @@ function meshtorus(innerradius, outerradius, h)
     gmsh.model.geo.synchronize()
     gmsh.option.setNumber("Mesh.MshFileVersion",2)
     gmsh.model.mesh.generate(2)
-    gmsh.fltk.run()
+    # gmsh.fltk.run()
     gmsh.write(fno)
     gmsh.finalize()
 
@@ -622,7 +622,8 @@ function meshsquaretorus(width, height, holewidth, h)
     gmsh.model.geo.addLine(2, 3, 2)
     gmsh.model.geo.addLine(3, 4, 3)
     gmsh.model.geo.addLine(4, 1, 4)
-
+    gmsh.model.geo.addCurveLoop([1, 2, 3, 4], 101)
+    
     gmsh.model.geo.addPoint(width/2, -width/2, -height/2, h, 5)
     gmsh.model.geo.addPoint(width/2, width/2, -height/2, h, 6)
     gmsh.model.geo.addPoint(-width/2, width/2, -height/2, h, 7)
@@ -631,20 +632,80 @@ function meshsquaretorus(width, height, holewidth, h)
     gmsh.model.geo.addLine(6, 7, 6)
     gmsh.model.geo.addLine(7, 8, 7)
     gmsh.model.geo.addLine(8, 5, 8)
-    gmsh.model.geo.addLine(1, 5, 9)
-    gmsh.model.geo.addLine(2, 6, 10)
-    gmsh.model.geo.addLine(3, 7, 11)
-    gmsh.model.geo.addLine(4, 8, 12)
-    gmsh.model.geo.addCurveLoop([1, 10, -5, -9], 13)
-    gmsh.model.geo.addSurfaceFilling([13], 1)
-    gmsh.model.geo.addCurveLoop([2, 11, -6, -10], 14)
-    gmsh.model.geo.addSurfaceFilling([14], 2)
-    gmsh.model.geo.addCurveLoop([3, 12, -7, -11], 15)
-    gmsh.model.geo.addSurfaceFilling([15], 3)
-    gmsh.model.geo.addCurveLoop([4, 9, -8, -12], 16)
-    gmsh.model.geo.addSurfaceFilling([16], 4)
 
-    gmsh.model.geo.extrude([(2, 1), (2, 2), (2, 3), (2, 4)], 0, 0, height)
+    gmsh.model.geo.addCurveLoop([5, 6, 7, 8], 102)
+    gmsh.model.geo.addPlaneSurface([101, 102], 1)
+    gmsh.model.geo.extrude([(2, 1)], 0, 0, height)
+
+    gmsh.model.geo.synchronize()
+    gmsh.option.setNumber("Mesh.MshFileVersion",2)
+    gmsh.model.mesh.generate(2)
+    # gmsh.fltk.run()
+    gmsh.write(fno)
+    gmsh.finalize()
+
+    m = read_gmsh_mesh(fno)
+    rm(fno)
+    return m
+end
+
+meshsquaretorus(;width, height, holewidth, h) = meshsquaretorus(width, height, holewidth, h)
+
+"""
+    meshsquaretorus4(width, height, holewidth, h)
+    meshsquaretorus4(;width, height, holewidth, h)
+
+Create a mesh of a square torus of size `width` and `height` with 4 holes of size `holewidth` and `height`
+
+The target edge size is `h`.
+"""
+function meshsquaretorus4(width, height, holewidth, h)
+    @assert (2*holewidth) < width
+    
+    fno = tempname() * ".msh"
+    gmsh.initialize()
+    gmsh.model.add("squaretorus4")
+
+    # bottom plate
+    gmsh.model.geo.addPoint(width/2, -width/2, -height/2, h, 1)
+    gmsh.model.geo.addPoint(width/2, width/2, -height/2, h, 2)
+    gmsh.model.geo.addPoint(-width/2, width/2, -height/2, h, 3)
+    gmsh.model.geo.addPoint(-width/2, -width/2, -height/2, h, 4)
+    gmsh.model.geo.addLine(1, 2, 1)
+    gmsh.model.geo.addLine(2, 3, 2)
+    gmsh.model.geo.addLine(3, 4, 3)
+    gmsh.model.geo.addLine(4, 1, 4)
+    gmsh.model.geo.addCurveLoop([1, 2, 3, 4], 101)
+
+    gmsh.model.geo.addPoint(holewidth/2 + width/4, -holewidth/2 - width/4, -height/2, h, 5)
+    gmsh.model.geo.addPoint(holewidth/2 + width/4, holewidth/2 - width/4, -height/2, h, 6)
+    gmsh.model.geo.addPoint(-holewidth/2 + width/4, holewidth/2 - width/4, -height/2, h, 7)
+    gmsh.model.geo.addPoint(-holewidth/2 + width/4, -holewidth/2 - width/4, -height/2, h, 8)
+    gmsh.model.geo.addLine(5, 6, 5)
+    gmsh.model.geo.addLine(6, 7, 6)
+    gmsh.model.geo.addLine(7, 8, 7)
+    gmsh.model.geo.addLine(8, 5, 8)
+
+    gmsh.model.geo.addCurveLoop([5, 6, 7, 8], 102)
+
+    hole2 = gmsh.model.geo.copy([(1, 5), (1, 6), (1, 7), (1, 8)])
+    gmsh.model.geo.translate(hole2, 0, width/2, 0)
+    curveloop2 = [tag for (dim, tag) in hole2]
+    gmsh.model.geo.addCurveLoop(curveloop2, 103)
+
+    hole3 = gmsh.model.geo.copy([(1, 5), (1, 6), (1, 7), (1, 8)])
+    gmsh.model.geo.translate(hole3, -width/2, width/2, 0)
+    curveloop3 = [tag for (dim, tag) in hole3]
+    gmsh.model.geo.addCurveLoop(curveloop3, 104)
+
+    hole4 = gmsh.model.geo.copy([(1, 5), (1, 6), (1, 7), (1, 8)])
+    gmsh.model.geo.translate(hole4, -width/2, 0, 0)
+    curveloop4 = [tag for (dim, tag) in hole4]
+    gmsh.model.geo.addCurveLoop(curveloop4, 105)
+
+    gmsh.model.geo.addPlaneSurface([101, 102, 103, 104, 105], 1)
+    gmsh.model.geo.extrude([(2, 1)], 0, 0, height)
+
     gmsh.model.geo.synchronize()
     gmsh.option.setNumber("Mesh.MshFileVersion",2)
     gmsh.model.mesh.generate(2)
@@ -657,7 +718,7 @@ function meshsquaretorus(width, height, holewidth, h)
     return m
 end
 
-meshsquaretorus(;width, height, holewidth, h) = meshsquaretorus(width, height, holewidth, h)
+meshsquaretorus4(;width, height, holewidth, h) = meshsquaretorus4(width, height, holewidth, h)
 
 """
     meshrectangle_unstructured(width, height, delta)
