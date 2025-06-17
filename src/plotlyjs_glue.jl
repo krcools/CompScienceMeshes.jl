39a77ba1777ea8df4822db67bfd5f2632775a117
Kristof Cools <krcools@gmail.com>
2025-06-17 10:12:55 +0200

    add mesh3d to Plotly glue to stay closer to Plotly syntax

--- src/plotlyjs_glue.jl
+++ src/plotlyjs_glue.jl
@@ -52,6 +52,46 @@ function __init__()
             return patch(mesh; kwargs...)
         end
 
+        # @eval function PlotlyBase.mesh3d(u::AbstractVector, U::BEAST.AbstractSpace; kwargs...)
+
+        #     fcr, geo = BEAST.facecurrents(u, U)
+
+        #     v = CompScienceMeshes.vertexarray(geo)
+        #     c = CompScienceMeshes.cellarray(geo)
+
+        #     x = v[:,1];    y = v[:,2];    z = v[:,3]
+        #     i = c[:,1].-1; j = c[:,2].-1; k = c[:,3].-1
+
+        #     return PlotlyBase.mesh3d(;
+        #         x=x, y=y, z=z,
+        #         i=i, j=j, k=k,
+        #         intensitymode="cell",
+        #         intensity=norm.(fcr),
+        #         kwargs...)
+        # end
+
+
+        # @eval PlotlyBase.mesh3d(u::BEAST.FEMFunction; kwargs...) = PlotlyBase.mesh3d(u.coeffs, u.space; kwargs...)
+
+
+        @eval function PlotlyBase.mesh3d(geo::CompScienceMeshes.AbstractMesh; kwargs...)
+
+            # fcr, geo = BEAST.facecurrents(u, U)
+
+            v = CompScienceMeshes.vertexarray(geo)
+            c = CompScienceMeshes.cellarray(geo)
+
+            x = v[:,1];    y = v[:,2];    z = v[:,3]
+            i = c[:,1].-1; j = c[:,2].-1; k = c[:,3].-1
+
+            return PlotlyBase.mesh3d(;
+                x=x, y=y, z=z,
+                i=i, j=j, k=k,
+                # intensitymode="cell",
+                # intensity=norm.(fcr),
+                kwargs...)
+        end
+
         @eval function cones(mesh, arrows; sizeref=2, kwargs...)
             # normals = [normal(chart(mesh,cell)) for cell in mesh]
             centers = [cartesian(center(chart(mesh,cell))) for cell in mesh]
