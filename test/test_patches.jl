c14313da89983796e136ec4a1ae1af195ebadec2
bboeyken <Brecht.Boeykens@UGent.be>
2026-02-02 20:34:47 +0100

    positive volume in 2D simplex

--- test/test_patches.jl
+++ test/test_patches.jl
@@ -1,6 +1,7 @@
 using Test
 using CompScienceMeshes
 
+# universe dimension 3
 for T in [Float32, Float64]
     local mesh = meshrectangle(T(1.0), T(1.0), T(1.0))
     local faces = skeleton(mesh, 2)
@@ -16,5 +17,23 @@ for T in [Float32, Float64]
         point(T, -1.0, 1.0, 0.0)]
     @test p.normals == [
         point(T, 0.0, 0.0, -1.0)]
+    @test p.volume == T(0.5)
+end
+
+# universe dimension 2
+for T in [Float32, Float64]
+    local mesh = meshrectangle(T(1.0), T(1.0), T(1.0),2)
+    local faces = skeleton(mesh, 2)
+    local verts = vertices(mesh, faces.faces[1])
+    p = simplex(verts)
+
+    @test p.vertices == [
+        point(T, 0.0, 0.0),
+        point(T, 0.0, 1.0),
+        point(T, 1.0, 0.0)]
+    @test p.tangents == [
+        point(T, -1.0, 0.0),
+        point(T, -1.0, 1.0)]
+    @test p.normals == []
     @test p.volume == T(0.5)
 end
