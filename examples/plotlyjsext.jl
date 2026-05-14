263ad2b92f56ead7de173e023e319a49c269e089
Kristof Cools <krcools@gmail.com>
2026-05-14 11:53:19 +0200

    fixed plotlyjsext example

--- examples/plotlyjsext.jl
+++ examples/plotlyjsext.jl
@@ -6,5 +6,5 @@ n = collect(normal(chart(m,c)) for c in m)
 
 tr1 = mesh3d(m; opacity=0.5)
 tr2 = cone(m, n; sizeref=2)
-tr3 = scatter3d(skeleton(m,1); width=2, color="black")
+tr3 = scatter3d(skeleton(m,1); line_width=4, line_color="black")
 Plot([tr1,tr2,tr3])
